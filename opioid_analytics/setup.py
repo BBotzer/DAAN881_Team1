e116912b152a1f910ef5da95e34099c52535e639
BBotzer <btb5103@gmail.com>
2024-05-10 15:36:03 -0400

    Initial commit

---
+++ opioid_analytics/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project to analyze opioid crisis information to better make informed prescriptive decisions.',
+    author='DAAN881_Group1',
+    license='MIT',
+)
